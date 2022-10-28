d4b44207444852fb56bdcb1703976afdc58c5d4e
v3lyv3l4 <112582718+v3lyv3l4@users.noreply.github.com>
2022-10-27 17:55:03 -0700

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,13 @@
+basic.forever(function () {
+    if (input.lightLevel() < 50) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.clearScreen()
+    }
+})
